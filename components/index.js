ab6f6e2a18fb12ad49eace682783d9402ffa48d1
SyntheticQuimera <107224722+SyntheticQuimera@users.noreply.github.com>
2022-06-18 23:20:17 -0400

    Update

---
+++ components/index.js
@@ -0,0 +1,6 @@
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+
+export { HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton };
